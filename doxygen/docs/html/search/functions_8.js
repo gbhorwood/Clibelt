30d1bdcc46895eecc11877f2690474453b74618e
gbh <frymaster@resist.ca>
2017-01-09 22:04:26 -0700

    api docs

---
+++ doxygen/docs/html/search/functions_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['safecopy',['safeCopy',['../class_clibelt.html#a14e38bbd822a154551b9a41411e95dc7',1,'Clibelt']]],
+  ['spin',['spin',['../class_clibelt.html#adf469d77973da15ab118206d2c84347e',1,'Clibelt']]],
+  ['strlenansisafe',['strlenAnsiSafe',['../class_clibelt.html#aacafa278b93d53cb1aa0ee2f8da3c31b',1,'Clibelt']]]
+];
