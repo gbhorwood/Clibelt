8ad75416ae8509af470cc3708f5e7aa52fe9aa9d
gbh <frymaster@resist.ca>
2017-01-17 16:54:22 -0700

    doxygens for println()

--- doxygen/docs/html/search/functions_7.js
+++ doxygen/docs/html/search/functions_7.js
@@ -3,6 +3,7 @@ var searchData=
   ['pad',['pad',['../class_clibelt.html#a9e99e911db513bb271f39c3369bbdb20',1,'Clibelt']]],
   ['prepargs',['prepArgs',['../class_clibelt.html#afc786fe851f45ce389a5393f71cbc724',1,'Clibelt']]],
   ['printerr',['printerr',['../class_clibelt.html#a9e75d2aa36480d5f62677da1a7fdcd83',1,'Clibelt']]],
+  ['printlist',['printlist',['../class_clibelt.html#aadc6928ff265f9dd6d438521f6894a5f',1,'Clibelt']]],
   ['printout',['printout',['../class_clibelt.html#ad66e12cb4cc3f7481670f2549881a545',1,'Clibelt']]],
   ['printoutmenubox',['printoutMenuBox',['../class_clibelt.html#ae60ce4bd81570b9a0fc882a95eb101e0',1,'Clibelt']]],
   ['printoutmenuboxcache',['printoutMenuBoxCache',['../class_clibelt.html#a2945a5ec498465fa7a42a9d4854411ec',1,'Clibelt']]],
